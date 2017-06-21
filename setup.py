a5f9ec0a9f021d091b5d72860de749d7e0732cb2
Alexander Kovalev <ak@alkov.pro>
2017-06-21 17:47:30 +0300

    trying new package structure

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ if 'TRAVIS_BUILD_NUMBER' in os.environ and 'TRAVIS_BRANCH' in os.environ:
         os.environ['TRAVIS_BUILD_NUMBER'],
     )
 
-    devStatus = '5 - Production/Stable' if 'release' in os.environ['TRAVIS_BRANCH'] or os.environ['TRAVIS_BRANCH'] == 'master' else devStatus
+    devStatus = '5 - Production/Stable' if 'release' in os.environ['TRAVIS_BRANCH'] or os.environ[
+                                                                                           'TRAVIS_BRANCH'] == 'master' else devStatus
 
 else:
     print("This is local build")
@@ -26,28 +27,17 @@ else:
 
 print("vspheretools build version = {}".format(__version__))
 
-
 setup(
     name='vspheretools',
-
     version=__version__,
-
     description='vSphereTools is a set of scripts from DevOpsHQ to support working with vSphere and virtual machines (VMs) on it, which are based on the pysphere library.',
-
     long_description='You can see detailed user manual here: https://devopshq.github.io/vspheretools/',
-
     license='MIT',
-
     author='Timur Gilmullin',
-
     author_email='tim55667757@gmail.com',
-
     url='https://devopshq.github.io/vspheretools/',
-
     download_url='https://github.com/devopshq/vspheretools.git',
-
-    entry_points={'console_scripts': ['vspheretools = VSphereTools:Main']},
-
+    entry_points={'console_scripts': ['vspheretools = vspheretools.VSphereTools:Main']},
     classifiers=[
         'Development Status :: {}'.format(devStatus),
         'Environment :: Console',
@@ -57,7 +47,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
     ],
-
     keywords=[
         'vsphere',
         'sphere client',
@@ -69,39 +58,27 @@ setup(
         'virtualization',
         'routines',
     ],
-
     packages=[
-        '.',
-    ],
+        'vspheretools',
+        'pysphere',
+        'pysphere.resources',
+        'pysphere.ZSI',
+        'pysphere.ZSI.generate',
+        'pysphere.ZSI.wstools',
 
+    ],
     setup_requires=[
     ],
-
     tests_require=[
         'pytest',
     ],
-
     install_requires=[
     ],
-
     package_data={
         '': [
-            './tests/*.py',
-
-            './pysphere/*.py',
-            './pysphere/resources/*.py',
-            './pysphere/ZSI/*.py',
-            './pysphere/ZSI/LBNLCopyright',
-            './pysphere/ZSI/generate/*.py',
-            './pysphere/ZSI/wstools/*.py',
-
-            'VSphereTools.py',
-            'Logger.py',
-
             'LICENSE',
             'README.md',
         ],
     },
-
     zip_safe=True,
 )
